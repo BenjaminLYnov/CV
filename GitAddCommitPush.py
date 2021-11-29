fe1bc59049618388209556ccbc1216f75fb08911
unknown <you@example.com>
2021-11-29 09:30:11 +0100

    last commit

--- GitAddCommitPush.py
+++ GitAddCommitPush.py
@@ -3,3 +3,4 @@ import os
 absFilePath = os.path.abspath(__file__)
 path, filename = os.path.split(absFilePath)
 os.system("cd " + path + " && git add * && git commit -m \"last commit\" && git push")
+input('\n\n\n\n\nAppuyer sur Entrée pour fermer...')
