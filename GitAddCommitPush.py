65a7a49a16c6ae73deea523f7ad538e18039af85
unknown <you@example.com>
2021-11-29 09:31:23 +0100

    last commit

--- GitAddCommitPush.py
+++
@@ -1,6 +0,0 @@
-import os
-
-absFilePath = os.path.abspath(__file__)
-path, filename = os.path.split(absFilePath)
-os.system("cd " + path + " && git add * && git commit -m \"last commit\" && git push --set-upstream origin master")
-input('\n\n\n\n\nAppuyer sur Entrée pour fermer...')
